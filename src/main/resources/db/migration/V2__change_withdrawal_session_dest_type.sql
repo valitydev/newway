0068cd6f3eddfcf46815bdd173da93dfefbe5898
Inal Arsanukaev <aiz@empayre.com>
2022-08-23 18:28:46 +0300

    Fix script

---
+++ src/main/resources/db/migration/V2__change_withdrawal_session_dest_type.sql
@@ -0,0 +1,2 @@
+ALTER TABLE dw.withdrawal_session ALTER COLUMN destination_card_payment_system TYPE VARCHAR;
+DROP TYPE dw.BANK_CARD_PAYMENT_SYSTEM;
--- src/main/resources/db/migration/V95__change_withdrawal_session_dest_type.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE nw.withdrawal_session ALTER COLUMN destination_card_payment_system TYPE VARCHAR;
-DROP TYPE nw.BANK_CARD_PAYMENT_SYSTEM;
