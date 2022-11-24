fafd4d396fe12557661f4cb81e67b001463387e7
Anatoly Karlov <karleowne@gmail.com>
2022-11-24 15:29:36 +0600

    add new limiter scopes

---
+++ src/main/resources/db/migration/V8__add_new_limiter_scopes.sql
@@ -0,0 +1,3 @@
+ALTER TYPE dw.limit_config_limit_scope_type ADD VALUE 'provider';
+ALTER TYPE dw.limit_config_limit_scope_type ADD VALUE 'terminal';
+ALTER TYPE dw.limit_config_limit_scope_type ADD VALUE 'payer_contact_email';
