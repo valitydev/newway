43b53e6512cd014dfb12889a56f7d679743c4ba7
Gregory <32060161+ggmaleva@users.noreply.github.com>
2023-09-20 18:08:50 +0300

    add rack property in consumer group from file (#105)
    Co-authored-by: ggmaleva <ggmaleva@yandex.ru>

---
+++ src/main/java/dev/vality/newway/service/FileService.java
@@ -0,0 +1,38 @@
+package dev.vality.newway.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class FileService {
+
+    public static final String DELIMITER = "=";
+
+    public String getClientRack(String path) {
+        if (Objects.isNull(path)) {
+            return null;
+        }
+        String rack = getProperty(path);
+        log.debug("Get kafka rack: {} for consumer group", rack);
+        return rack;
+    }
+
+    private String getProperty(String stringFilePath) {
+        try {
+            var path = Paths.get(stringFilePath);
+            String content = Files.readString(path);
+            return content.split(DELIMITER)[1];
+        } catch (IOException e) {
+            log.error("Can't parse property from path: {}", stringFilePath, e);
+            return null;
+        }
+    }
+
+
+}
